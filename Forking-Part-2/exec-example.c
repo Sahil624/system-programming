7afe5ae604767af7de85789d2a0f081a657eec11
Sahil <sahilmangotra1997@gmail.com>
2017-03-13 16:34:26 +0000

    Forking Part 2 done

---
+++ Forking-Part-2/exec-example.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <fcntl.h> // O_CREAT, O_APPEND etc. defined here
+#include<string.h>
+
+int main() {
+   close(1); // close standard out
+   open("log.txt", O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+   puts("Captain's log");
+   chdir("/usr/include");
+   // execl( executable,  arguments for executable including program name and NULL at the end)
+
+   execl("/bin/ls", /* Remaining items sent to ls*/ "/bin/ls", ".", (char *) NULL); // "ls ."
+   perror("exec failed");
+   return 0; // Not expected
+}
