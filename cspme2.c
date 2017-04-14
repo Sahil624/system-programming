72bb469048c5e7b1c8ed09f922d4f44e1f16be0d
Sahil <sahilmangotra1997@gmail.com>
2017-04-14 14:36:22 +0000

    Critical section for 2 process

--- cspme2.c
+++ cspme2.c
@@ -3,29 +3,6 @@
 #include<pthread.h>
 
 long long sum =0;
-int flag[2];
-int turn;
-const int MAX = 10000;
-int ans = 0;
-
-
-void lock_init(){
-    flag[0]=flag[1]=0;
-    turn = 0;
-}
-
-void lock(int self){
-    flag[self]=1;
-    turn = 1-self;
-    
-    while(flag[1-self]==1 && turn == 1-self);
-}
-
-void unlock(int self){
-    flag[self]=0;
-}
-
-
 
 void* sum_runner(void* arg){
     long long *limitptr = (long long*) arg;
@@ -45,19 +22,16 @@ int main(int argc, char **argv){
         exit(-1);
     }
     
-   // long long limit = atoll(argv[1]);
-   // long long limit2 = atoll(argv[2]);
+    long long limit = atoll(argv[1]);
     
-    pthread_t tid,tid2;
+    pthread_t tid;
     
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     
     pthread_create(&tid,&attr,sum_runner,&limit);
-    pthread_create(&tid2,&attr,sum_runner,&limit2);
     
     pthread_join(tid,NULL);
-     pthread_join(tid2,NULL);
     
     printf("%lld\n",sum);
     //fork();
